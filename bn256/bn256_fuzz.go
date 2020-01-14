acdb4cc5d394e0063ca26d3f155d42424c1b1dff
alpha <alphaqiu@gmail.com>
2020-01-14 10:56:16 +0800

    change import path

--- bn256/bn256_fuzz.go
+++ bn256/bn256_fuzz.go
@@ -10,8 +10,8 @@ import (
 	"bytes"
 	"math/big"
 
-	cloudflare "github.com/boxproject/bolaxy/crypto/bn256/cloudflare"
-	google "github.com/boxproject/bolaxy/crypto/bn256/google"
+	cloudflare "github.com/bolaxy/crypto/bn256/cloudflare"
+	google "github.com/bolaxy/crypto/bn256/google"
 )
 
 // FuzzAdd fuzzez bn256 addition between the Google and Cloudflare libraries.
